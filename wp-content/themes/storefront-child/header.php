b30bdf84b9af56f3a939d36631af2f518b58c522
Larry Mesmer <reallymute@gmail.com>
2018-02-24 15:03:20 +0100

    Script 6 with jQuery

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -16,20 +16,26 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script >
 $.fn.center = function () {
-this.css("position","absolute");
-this.css("top", Math.max(0, (
+   // alert("in center");
+   // src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+this.css("position","fixed");
+/* this.css("top", Math.max(0, (
 ($(window).height() - $(this).outerHeight()) / 2) + 
 $(window).scrollTop()) + "px"
-);
+);*/
+this.css("top", Math.max(0, (
+(($(window).height() / 2) - $(this).outerHeight()) / 2)
++ "px"
+));
 this.css("left", Math.max(0, (
-($(window).width() - $(this).outerWidth()) / 2) + 
-$(window).scrollLeft()) + "px"
+($(window).width() - $(this).outerWidth()) / 2)) + "px"
 );
+this.css("width", Math.max(0, ($(window).width() / 3) + "px"));
 return this;
-}
+};
 function zuu() {
 $("#billy").html("got click");
   $("#mp_splash").fadeOut();
@@ -50,13 +56,12 @@ z-index:1001;
 }
 #mp_splash{ 
 position: absolute;
+top: 20%;
+left: 20%;
 display: none;
 width: 600px;
 height: 300px;
 padding: 16px;
-border: 16px solid orange;
-background-color: white;
-background-image: http://icons.iconarchive.com/icons/cornmanthe3rd/plex/128/System-settings-icon.png;
 z-index:1002;
 }
 </style>
@@ -139,16 +144,20 @@ z-index:1002;
 	do_action( 'storefront_before_content' ); ?>
 <p id="billy" >Cliquez sur la zone grisée</p>
 <div id="mp_splashoverlay" > </div>
-<div id="mp_splash" >Bienvenu dans FintecX
+<div id="mp_splash" ><h3 > <font color="white">Bienvenu dans FintecX</font></h3>
+    <img width="128" height="128" src="http://t3v1.axerex.fr/wp-content/uploads/2018/02/Letter-P-icon.png" class="image wp-image-115  attachment-full size-full" alt="" style="max-width: 100%; height: auto;">
 </div>
-<script>
+<script >
 //$("#mp-splashoverlay").show();
-//$("#mp-splash").show().center();
 
 setTimeout(function(){ 
 $("#mp_splashoverlay").fadeIn();
 }, 3000);
 setTimeout(function(){ 
+    var mps = $("#mp-splash");
+//$("#mp-splash").center();
+mps.css("position","fixed");
+mps.css("width", Math.max(0, ($(window).width() / 3) + "px"));
 $("#mp_splash").fadeIn();
 }, 3500);
 $("#mp_splash").click(function(){
@@ -160,7 +169,7 @@ $("#mp_splashoverlay").click(function(){
   //  		div.animate({height: '200px'}, "slow");
        
   //     div.html(div.html+ "<br>" + div.attr("height"));
-       $("#billy").html($("#mp_splash").height() + " / " + $("#mp_splash").css("display"));
+       $("#billy").html($("#mp_splash").height() + " / " + $("#mp_splash").css("display") + " /windowheight: " + $(window).height()+ " /splash position " + $("#mp_splash").css("position") + " /window width " + $(window).width());
 setTimeout(function(){ 
 $("#mp_splashoverlay").fadeOut();
 }, 300);
