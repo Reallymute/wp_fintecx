0b8694e3df0f9cfbf195d856962a1476e1bb7e10
Larry Messmer on Vostro860 <larmess@freesurf.fr>
2018-04-18 12:16:52 +0200

    Le widget marche je sais pas

--- wp-content/plugins/fintecx/templates/widget/fintecx_iamnotarobot.php
+++ wp-content/plugins/fintecx/templates/widget/fintecx_iamnotarobot.php
@@ -12,20 +12,24 @@ extract( $instance );
 global $fintecxPlugin;
 //  $fintecxPlugin is instanciated in fintecx-plugin.php
 
-?><div class="fintecx-iamnotarobot-wrapper">
-		<div class="flipthis" ></div>
-		<a href="<?php echo $qrcode ?>" target="_blank"><?php _e( 'Cliquez Ici <br> Widget FintecX I am not a robot', 'fintecx' ) ?></a>
-                <div class="example switch">
-  <div>
-    <p class="turnBoxButton">OFF</p>
-  </div>
-  <div>
-    <p class="turnBoxButton turnBoxButtonPrev">ON</p>
-  </div>
-</div>
-
-</div>
+?>
 <?php 
+
+wp_add_inline_script( 'flipit', 
+  'jQuery(".flipthis").on("click",function() {
+    turnBox({
+    width:200,
+    height:50,
+    axis:"X",
+    perspective:800,
+    duration:200,
+    delay:0,
+    easing:"linear",
+    direction:"positive",
+    type:"real"
+      });
+       } );');
+/*
 wp_add_inline_script( 'flipit', 
   '(function ($) {
     $(".flipthis").turnBox({
@@ -41,3 +45,66 @@ wp_add_inline_script( 'flipit',
     
        });
 })(jQuery)');
+*/
+
+?>
+
+<div class="fintecx-iamnotarobot-wrapper">
+		<div  ></div>
+		<a href="<?php echo $qrcode ?>" target="_blank"><?php _e( 'Cliquez Ici <br> Widget FintecX I am not a robot', 'fintecx' ) ?></a>
+<div class="flipthis" >
+  <div>
+    <p class="turnBoxButton">OFF</p>
+  </div>
+  <div>
+    <p class="turnBoxButton turnBoxButtonPrev">ON</p>
+  </div>
+    <button id="qrcode_btn">Convertir en QRcode</button>
+    <h1 id="control1">Control text</h1>
+</div>
+
+</div>
+<script type="text/javascript">
+    
+/*
+ * on attend ceci
+ * { "json_content" : 
+ *      { "server_response" : "UVJfRU5DT0RFO01lcmNoYW50LElEPTc5NzIzNjgzNzQ2ODYy
+ *      Mzg3NDY0fEFVVEhUT0tFTjtUb2tlbj0xMDA5NjAzNXxRUkNPREVfRklMRV9OQU1FO0ZpbGV
+ *      uYW1lPURFZmF1bHQgRmlsZXxRUkNPREVfUkVTVUxUX1VSTDtVUkw9fEVORF9RU
+ *      l9FTkNPREU=Server : reallymute-app.herokuapp.com", 
+ *      "received_query" : "QRCODE_GENERATOR" , 
+ *      "json_security_token" : "AB67844D981EF554314F20180321174358" 
+ *      } 
+ *  }    
+    */
+   
+             jQuery(document).ready(function ($) {alert("After click");
+             
+        $("#qrcode_btn").click(function() {
+             alert("inside click");
+       
+        $.getJSON("<?php echo $qrcode ?>",function(data,status) {
+            var contr = document.getElementById('control1');
+            contr.innerHTML = data.json_content.server_response;
+        });
+    });
+    });
+
+    
+    /*
+    $(".flipthis").on("click",function() {
+    $(".turnBoxButton").turnBox({
+    width:200,
+    height:50,
+    axis:"X",
+    perspective:800,
+    duration:200,
+    delay:0,
+    easing:"linear",
+    direction:"positive",
+    type:"real"
+      });
+       } ); 
+       */
+          </script>
