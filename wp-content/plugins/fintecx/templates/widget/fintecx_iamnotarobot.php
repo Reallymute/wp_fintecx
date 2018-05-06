ef3a0456d3f9c7c9655ab00bfe9f3ad0ae2b5838
Larry Messmer on Vostro860 <larmess@freesurf.fr>
2018-05-06 15:33:32 +0200

    Le widget marche je sais pas3

--- wp-content/plugins/fintecx/templates/widget/fintecx_iamnotarobot.php
+++ wp-content/plugins/fintecx/templates/widget/fintecx_iamnotarobot.php
@@ -86,15 +86,16 @@ wp_add_inline_script( 'flipit',
         $("#qrcode_btn").click(function() {
              alert("inside click");
        document.getElementById('control1').innerHTML="Ready For Click result";
-       $.ajaxSetup({
+  /*     $.ajaxSetup({
   headers : {   
     'Access-Control-Allow-Origin' : '*'
   }
-});
-        $.getJSON("<?php echo $qrcode ?>",function(data,status) {
+}); */
+        $.getJSON("<?php echo $qrcode ?>",function(data) {
             var contr = document.getElementById('control1');
+            console.log(data);
             contr.innerHTML = data.json_content.server_response;
-        });
+        }, function(data,status,jqXHR){console.log(status);});
         var conuuu = document.getElementById('control1');
         conuuu.innerHTML="getJSON was processed... more 06may";
 
